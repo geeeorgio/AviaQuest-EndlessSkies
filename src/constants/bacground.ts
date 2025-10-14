b786e196081069c39e325f0fa9e2ed943fb5b0a4
george <heorhii.kyryliuk@gmail.com>
2025-10-14 03:38:22 +0300

    fix

--- src/constants/bacground.ts
+++ src/constants/bacground.ts
@@ -1,4 +1,4 @@
 export const MAIN_BACKGROUND = require('../assets/images/background/appbg.png');
 
 export const GAME_BACKGROUND_ORIGINAL = require('../assets/images/background/gameBg.png');
-export const GAME_BACKGROUND_MIRRORED = require('../assets/images/background/gameBg.png');
+export const GAME_BACKGROUND_MIRRORED = require('../assets/images/background/gameBg-mirroed.png');
